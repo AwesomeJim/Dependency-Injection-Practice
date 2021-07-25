a984aaaf283b8c237a0da8380ef19c63d0f51b0d
awesomejim <jimnahgua@gmail.com>
2021-07-25 07:04:05 +0300

    - Completed Basic Hilt implementation

--- HiltKotlin/src/main/java/com/example/android/hilt/ServiceLocator.kt
+++ HiltKotlin/src/main/java/com/example/android/hilt/ServiceLocator.kt
@@ -25,6 +25,16 @@ import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.AppNavigatorImpl
 import com.example.android.hilt.util.DateFormatter
 
+/**
+ * A container is a class which is in charge of providing dependencies in your codebase and knows how to create instances of other types in your app.
+ * It manages the graph of dependencies required to provide those instances by creating them and managing their lifecycle.
+ * A container exposes methods to get instances of the types it provides. Those methods can return a new instance every time or always return the same instance.
+ * If the method always provides the same instance, we say that the type is scoped to the container.
+
+ * @property logsDatabase AppDatabase
+ * @property loggerLocalDataSource LoggerLocalDataSource
+ * @constructor
+ */
 class ServiceLocator(applicationContext: Context) {
 
     private val logsDatabase = Room.databaseBuilder(
