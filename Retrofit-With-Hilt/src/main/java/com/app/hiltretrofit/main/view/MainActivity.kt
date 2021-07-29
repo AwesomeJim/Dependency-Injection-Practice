8ff79bf0a88e506a6bbb300d6c38209653613cea
awesomejim <jimnahgua@gmail.com>
2021-07-29 11:25:11 +0300

    Updated gradle plugins

--- Retrofit-With-Hilt/src/main/java/com/app/hiltretrofit/main/view/MainActivity.kt
+++ Retrofit-With-Hilt/src/main/java/com/app/hiltretrofit/main/view/MainActivity.kt
@@ -10,11 +10,17 @@ import com.app.hiltretrofit.databinding.ActivityMainBinding
 import com.app.hiltretrofit.main.view.adapter.CountryAdapter
 import com.app.hiltretrofit.main.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
+
+    //inject the country Adaptor
+    @Inject
+    lateinit var countryAdapter: CountryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -27,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         binding.countryRecyclerview.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             setHasFixedSize(true)
-            adapter = CountryAdapter()
+            adapter = countryAdapter
             addItemDecoration(DividerItemDecoration(this@MainActivity, LinearLayout.VERTICAL))
         }
     }
@@ -36,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         mainViewModel.getCountry().observe(this, { countryList ->
             countryList?.let {
                 binding.countryRecyclerview.apply {
-                    with(adapter as CountryAdapter) {
+                    with(countryAdapter) {
                         countries = it
                         notifyDataSetChanged()
                     }
